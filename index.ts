6c33981aa7d0a9016841f7c935ca2904028229ed
Dmitry <p.dmitry.y@gmail.com>
2022-12-23 22:02:16 +0300

    feat: connect sqllite

--- index.ts
+++ index.ts
@@ -1,5 +1,23 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import * as sqlite from 'sqlite3';
+
+const sqlite3 = sqlite.verbose();
+let db = new sqlite3.Database('./db/data.db', (err) => {
+  if(err) {
+    console.log(err.message)
+  };
+  console.log('Connected to the database');
+});
+
+db.close((err) => {
+  if(err) {
+    console.log(err.message)
+  };
+  console.log('Database connection was closed');
+})
+
+
 
 dotenv.config();
 
